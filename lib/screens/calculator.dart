3e459084ccd2625b0c8651e3e1a656524dc9ec36
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-04-27 20:15:29 -0300

    Construindo a Interface Gráfica

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../models/memory.dart';
+import '../components/display.dart';
+import '../components/keyboard.dart';
+
+class Calculator extends StatefulWidget {
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: <Widget>[
+          Display(memory.value),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
